cd71cf5006db6838419217c83870241753223e65
Kashan-2912 <mkashan2912@gmail.com>
2025-08-13 17:09:14 +0500

    updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative w-full overflow-x-hidden  ">
+      
       <div className="relative">
         <ImgCarousel />
       </div>
