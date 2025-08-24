5dedb232d7dd81fd63689a98875ab4425c4475ce
Kashan-2912 <mkashan2912@gmail.com>
2025-08-24 17:31:50 +0500

    text update

--- src/components/coownership/CoOwnerShipModel.jsx
+++ src/components/coownership/CoOwnerShipModel.jsx
@@ -6,7 +6,7 @@ const CoOwnerShipModel = () => {
   return (
     <div id='ownership-model' className="flex flex-col items-center px-4 sm:px-6 lg:px-8 mb-12">
       <p className="text-2xl sm:text-2xl md:text-4xl lg:text-5xl text-center text-[#37584F] font-bold gravesendSans mb-6 sm:mb-12">
-        Smart Propery Unit<br />Payment Plan
+        Smart Property Unit<br />Payment Plan
       </p>
       
       <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 items-center justify-center w-full max-w-7xl">
