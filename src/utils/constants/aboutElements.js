3057f5a1cc9044a06d9b61ae2bdc17d5289e457c
Muhammad Kashan Ashraf <168653347+Kashan-2912@users.noreply.github.com>
2025-10-16 18:10:00 +0500

    removed 2 images
    Updated image widths in aboutElements.js and commented out unused images.

--- src/utils/constants/aboutElements.js
+++ src/utils/constants/aboutElements.js
@@ -5,17 +5,17 @@ export const imageGroups = [
     { src: "aboutImg3.png", width: "25%" },
   ],
   [
-    { src: "aboutImg4.png", width: "26%" },
-    { src: "aboutImg5.png", width: "49%" },
-    { src: "aboutImg6.png", width: "25%" },
+    { src: "aboutImg4.png", width: "35%" },
+    { src: "aboutImg5.png", width: "65%" },
+    // { src: "aboutImg6.png", width: "25%" },
   ],
 ];
 
 export const sideImage = { src: "about7.png", width: "100%" };
 
 export const bottomImages = [
-  { src: "aboutImg8.png", width: "20%" },
-  { src: "aboutImg9.png", width: "20%" },
+  // { src: "aboutImg8.png", width: "20%" },
+  { src: "aboutImg9.png", width: "30%" },
   { src: "aboutImg10.png", width: "39%" },
-  { src: "aboutImg11.png", width: "20%" },
+  { src: "aboutImg11.png", width: "30%" },
 ];
