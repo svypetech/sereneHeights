fa0145405357eeead2f2778f4eaabaa6e70cb8ed
Muhammad Kashan Ashraf <168653347+Kashan-2912@users.noreply.github.com>
2025-10-20 18:43:23 +0500

    updated pricintTableData

--- src/utils/constants/pricingTableData.js
+++ src/utils/constants/pricingTableData.js
@@ -2,8 +2,8 @@ const tableData1 = [
     { description: "1 unit size sq ft", value: "50" },
     { description: "1 unit value", value: "2,000,000" },
     { description: "Down payment (30%)", value: "600,000" },
-    { description: "Possession (10%)", value: "200,000" },
-    { description: "4 bi-annual payment\nPkr 75,000 each", value: "300,000" },
+    // { description: "Possession (10%)", value: "200,000" },
+    // { description: "4 bi-annual payment\nPkr 75,000 each", value: "300,000" },
     { description: "36 monthly installments", value: "38,900" }
 ];
 
@@ -11,8 +11,8 @@ const tableData2 = [
     { description: "Investment amount", value: "2,000,000" },
     { description: "Expected annual rental income", value: "300,000" },
     { description: "Annual free night voucher", value: "2" },
-    { description: "Value package", value: "500,000" },
-    { description: "Annual capital gain on\ninvestment", value: "5%" },
+    // { description: "Value package", value: "500,000" },
+    // { description: "Annual capital gain on\ninvestment", value: "5%" },
     { description: "ROI", value: "20%" }
 ];
 
